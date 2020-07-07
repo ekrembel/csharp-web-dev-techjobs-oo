70afc6a7a04d5851d020741d9de2a0d42a87252f
Ekrem Bel <ekrem@Ekrems-MacBook-Pro-2.local>
2020-07-07 11:19:22 -0500

    Completed unit testing

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Job job1 = new Job();
+            Job job2 = new Job();
+            int jobOneId = job1.Id;
+            int jobTwoId = job2.Id;
+            Assert.IsTrue(jobTwoId == jobOneId + 1);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual("Product tester", job.Name);
+            Assert.AreEqual("ACME", job.EmployerName.Value);
+            Assert.AreEqual("Desert", job.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job.JobType.Value);
+            Assert.AreEqual("Persistence", job.JobCoreCompetency.Value);
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+            Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+            Assert.IsFalse(job1.Id == job2.Id);
+        }
+
+    }
+}
